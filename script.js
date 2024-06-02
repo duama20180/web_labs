98a583e1beb3ea66d94089446a6f628bf1865f6b
duama20180 <accforwork20180@gmail.com>
2024-06-02 15:15:21 +0300

    added black theme slider

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-    
-
-document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('DOMContentLoaded', () => {
     const currentTheme = localStorage.getItem('theme');
+    const themeSlider = document.getElementById('theme-slider');
 
     if (currentTheme === 'dark') {
         document.body.classList.add('dark-mode');
+        themeSlider.checked = true;
     }
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'd' || event.key === 'D' || event.key === 'В'|| event.key === 'в') {
-            document.body.classList.toggle('dark-mode');
-            let theme = 'light';
-            if (document.body.classList.contains('dark-mode')) {
-                theme = 'dark';
-            }
-            localStorage.setItem('theme', theme);
+    themeSlider.addEventListener('change', () => {
+        document.body.classList.toggle('dark-mode');
+        let theme = 'light';
+        if (document.body.classList.contains('dark-mode')) {
+            theme = 'dark';
         }
+        localStorage.setItem('theme', theme);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('registrationForm');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const phone = document.getElementById('phone').value;
+
+        // Тут можна додати функціонал для обробки даних, наприклад, відправити їх на сервер
+        console.log(`Ім'я: ${firstName}, Прізвище: ${lastName}, Номер телефону: ${phone}`);
+
+        alert('Реєстрація успішна!');
+        form.reset();
     });
 });
