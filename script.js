87051c70c7376036222309636a090895dccc5471
duama20180 <accforwork20180@gmail.com>
2024-06-07 11:27:20 +0300

    adaptive menu added

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-    document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
+    // адаптив меню
+    const excursionsButton = document.querySelector('.excursions-button > a');
+    const dropdown = excursionsButton.nextElementSibling;
+
+    excursionsButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+    });
+
+    document.addEventListener('click', function(e) {
+        if (!excursionsButton.contains(e.target) && !dropdown.contains(e.target)) {
+            dropdown.style.display = 'none';
+        }
+    });
+
+
+    document.querySelector('.menu-button').addEventListener('click', function() {
+        document.querySelector('.links').classList.toggle('active');
+    });
+
+    // Темна тема
     const currentTheme = localStorage.getItem('theme');
     const themeSlider = document.getElementById('theme-slider');
 
@@ -15,21 +36,19 @@
         }
         localStorage.setItem('theme', theme);
     });
-});
 
-
+    // Модальне вікно
     const registerCircle = document.getElementById('register-circle');
     const modal = document.getElementById('modal');
     const closeModal = document.getElementById('close');
     const submit = document.getElementById('submit');
-
 
     registerCircle.addEventListener('click', () => {
         modal.style.display = 'block';
     });
 
     closeModal.addEventListener('click', () => {
-            modal.style.display = 'none';
+        modal.style.display = 'none';
     });
 
     window.addEventListener('click', (event) => {
@@ -38,35 +57,17 @@
         }
     });
 
-    submit.addEventListener('click', ()=> {
+    submit.addEventListener('click', () => {
         alert('Успішно заброньовано!');
         modal.style.display = 'none';
-    })
- 
+    });
+
+    // Знижка
     setTimeout(() => {
         document.getElementById('discount').style.display = 'block';
-    }, 120000);
-    
+    }, 12000000);
 
     document.getElementById('close-discount').onclick = function() {
         document.getElementById('discount').style.display = 'none';
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    };
+});
